a98df200690959fee1e79d440fa31e3a064e78d3
kdomo <dongho5088@naver.com>
2023-12-15 21:25:37 +0900

    [No2] feat: 계산 결과 출력

---
+++ src/main/java/no2/Main.java
@@ -0,0 +1,9 @@
+package no2;
+
+public class Main {
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		int result = calculator.add(4).add(5).subtract(3).out();
+		System.out.println(result);
+	}
+}
