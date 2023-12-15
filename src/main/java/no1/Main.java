dffeff5f432206e18c21c3bbdf74c7103fd4cfd3
kdomo <dongho5088@naver.com>
2023-12-15 21:18:15 +0900

    [No1] feat: JSONArray를 통해 객체 파싱하기

--- src/main/java/no1/Main.java
+++ src/main/java/no1/Main.java
@@ -14,6 +14,7 @@ import org.json.simple.parser.ParseException;
 public class Main {
 	public static void main(String[] args) throws IOException, ParseException {
 		JSONArray jsonArray = getJsonArray();
+		parsing(jsonArray);
 	}
 
 	private static JSONArray getJsonArray() throws IOException, ParseException {
@@ -22,5 +23,27 @@ public class Main {
 		Reader reader = new FileReader("src/main/resources/board.json");
 		JSONArray jsonArray = (JSONArray)parser.parse(reader);
 		return jsonArray;
+	}
+
+	private static List<Bread> parsing(JSONArray jsonArray) {
+		List<Bread> breads = new ArrayList<>();
+		for (Object obj : jsonArray) {
+			JSONObject bread = (JSONObject) obj;
+			String breadType = String.valueOf(bread.get("breadType"));
+
+			List<Recipe> recipes = new ArrayList<>();
+			JSONObject recipe = (JSONObject) bread.get("recipe");
+			if (recipe != null) {
+				for (Object ingredientKey : recipe.keySet()) {
+					String ingredient = (String) ingredientKey;
+					Long amount = (Long) recipe.get(ingredient);
+
+					recipes.add(Recipe.of(ingredient, amount));
+				}
+			}
+
+			breads.add(Bread.of(breadType, recipes));
+		}
+		return breads;
 	}
 }
